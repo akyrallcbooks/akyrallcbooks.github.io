904af1ae93ece9f14493d48dac72747ce271f462
Roop <bhargawr@caloes.ca.gov>
2020-02-10 16:23:06 -0800

    Buy Button
    Buy Button

--- data/posts.js
+++ data/posts.js
@@ -2,14 +2,15 @@ var posts = [{
         "id": "6",
         "description": "This book is about the newly merged phenomenon “Q” and will describe it in detail in former sections. The book will also make an attempt to establish a connection among “Q” and the “deep state” and will answer the question such as, “How Q is systematically destroying the deep state?”",
         "img": "./data/image/Q.jpg",
-        "currentdate": "02/10/2020"
-
+        "currentdate": "02/10/2020",
+        "link": "https://www.amazon.com/dp/B083ZNQ7DJ"
 
     }, {
         "id": "5",
         "description": "What happens to the Earth when AI has destroyed most of the planet? Is there hope for Humanity? Find out now with the second book in the series! AICalypto II.",
         "img": "./data/image/AI_2.jpg",
-        "currentdate": "02/10/2020"
+        "currentdate": "02/10/2020",
+        "link": "https://www.amazon.com/dp/B07NJ82G9T"
 
 
     },
@@ -17,14 +18,16 @@ var posts = [{
         "id": "4",
         "description": "An AI so powerful that it derived thousands of years of human knowledge of the game before inventing better moves of its own, all in the space of 7 days and destroys the planet",
         "img": "./data/image/AI.jpg",
-        "currentdate": "02/10/2020"
+        "currentdate": "02/10/2020",
+        "link": "https://www.amazon.com/dp/B07FB3WNK6"
 
 
     }, {
         "id": "3",
         "description": "If you are struggling to lose that belly fat, no matter how much you exercise and eat healthy, Intermittent fasting will help you in getting rid of unwanted fat. It is easy to fit Intermittent Fasting in your schedule and this book will show you how to achieve that.",
         "img": "./data/image/BellyFat.jpg",
-        "currentdate": "02/10/2020"
+        "currentdate": "02/10/2020",
+        "link": "https://www.amazon.com/dp/B078VTYVWZ"
 
 
     },
@@ -32,7 +35,8 @@ var posts = [{
         "id": "2",
         "description": "With the current nasty climate in Washington, DC and in between power struggles, America will be fed up with the games of Machoism by 2024 and would prefer a caring attitude of a woman to handle the day-to-day business of American politics. Ivanka would be a great fit for the Presidency of the USA.",
         "img": "./data/image/Ivanka.jpg",
-        "currentdate": "02/10/2020"
+        "currentdate": "02/10/2020",
+        "link": "https://www.amazon.com/dp/B074JZ5XKD"
 
 
     },
@@ -40,7 +44,8 @@ var posts = [{
         "id": "1",
         "description": "Conor has all the tools necessary to beat Floyd. With his unusual style and movement, Conor will upset the greatest boxer of all time Floyd Mayweather. This books illustrates the reason behind it.",
         "img": "./data/image/Conor.jpg",
-        "currentdate": "02/10/2020"
+        "currentdate": "02/10/2020",
+        "link": "https://www.amazon.com/dp/B074MLGRXB"
 
 
     }
